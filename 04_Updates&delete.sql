3784736f8159751ed6b45186bd43e456a4ef750d
KevinSV96 <kasc_3@hotmail.com>
2025-10-12 17:17:32 -0600

    Update 04_Updates&delete.sql

--- 04_Updates&delete.sql
+++ 04_Updates&delete.sql
@@ -1,7 +1,7 @@
 --- Modifique la fecha de ingreso a 13 de abril 2022 y la cantidad disponible a 10 de la película deACE Ventura(en una sola consulta)
 
 UPDATE peliculas
-SET fecha_ingreso = '2003-02-12', disponible = disponible + 5
+SET fecha_ingreso = '2003-02-12', disponible = disponible + 7
 WHERE codp = 'P001';
 
 --- Elimine las rentas que realizó Pedro Arias Rivas Cisneros
@@ -11,5 +11,5 @@ WHERE DUI = '01900683-1';  -- Pedro Arias
 
 --- Juegos to games:
 UPDATE categorias
-SET categorias = "Juegos"
+SET categorias = "Games"
 WHERE CodC = "C006"
